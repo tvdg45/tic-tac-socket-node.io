4be981d108cebf425745b17838db2a7980d2c833
Tim van der Graaff <timvdg45@gmail.com>
2018-07-20 12:22:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,6 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  
+  
 });
-
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
