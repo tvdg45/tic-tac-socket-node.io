f6d80275def544aba55600b9d1e4a56b580a4d9c
Tim van der Graaff <timvdg45@gmail.com>
2018-07-20 11:51:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,12 @@ const server = express()
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  
+	client.on('load_threads', function(data) {
+		
+		client.emit('load_threads', data);
+		client.broadcast.emit('load_threads', data);
+	});
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
