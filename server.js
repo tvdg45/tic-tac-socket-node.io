a6e41e1eb0609b3dbb55981d0acf964561df4f82
Tim van der Graaff <timvdg45@gmail.com>
2018-07-20 12:16:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,21 @@
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+'use strict';
 
-app.use((req, res, next) => {
-	
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials", "true");
-	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-	res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-	
-	next();
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-app.get('/', function(req, res, next) {
-	
-	res.sendFile(__dirname + '/index.html')
-});
-
-app.use(express.static('public'));
-
-io.on('connection', function(client) {
-	
-	client.on('load_threads', function(data) {
-		
-		client.emit('load_threads', data);
-		client.broadcast.emit('load_threads', data);
-	});
-
-  	client.on('change_threads', function(data) {
-		
-  		client.emit('change_threads', data);
-  		client.broadcast.emit('change_threads', data);
-  	});
-	
-  	client.on('erase_threads', function(data) {
-		
-  		client.emit('erase_threads', data);
-  		client.broadcast.emit('erase_threads', data);
-  	});
-	
-	client.on('create_threads', function(data) {
-		
-		client.emit('create_threads', data);
-		client.broadcast.emit('create_threads', data);
-	});
-});
-
-server.listen(1000);
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
