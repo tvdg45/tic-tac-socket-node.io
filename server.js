d2beaab6bede86113c658546ac8ac5e4d5938a76
Tim van der Graaff <timvdg45@gmail.com>
2018-07-20 10:20:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.php');
+const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
