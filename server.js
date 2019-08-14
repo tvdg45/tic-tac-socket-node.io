2388051bbf74b70ee259b9d185fcd50dd6c96536
Tim van der Graaff <timvdg45@gmail.com>
2019-08-14 16:16:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -13,23 +13,40 @@ const server = express()
 
 const io = socketIO(server);
 
+function forceGC() {
+
+    if (global.gc) {
+    	
+	global.gc();
+    } else {
+     	
+	console.warn('No GC hook! Start your program as `node --expose-gc server.js`.');
+    }
+}
+
 io.on('connection', function (client) {
 
 	client.on('refresh_admin_window', function(data) {
 		
 		client.emit('refresh_admin_window', data);
 		client.broadcast.emit('refresh_admin_window', data);
+		
+		forceGC();
 	});
 	
 	client.on('log_other_users_out', function(data) {
 		
 		client.emit('log_other_users_out', data);
 		client.broadcast.emit('log_other_users_out', data);
+		
+		forceGC();
 	});
 	
 	client.on('load_threads', function(data) {
 		
 		client.emit('load_threads', data);
 		client.broadcast.emit('load_threads', data);
+		
+		forceGC();
 	});
 });
